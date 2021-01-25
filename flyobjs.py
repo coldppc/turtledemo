c9250e130039a8eec4740c2a1c4cb2f7310b8d17
sxfan <fanshaoxing@gmail.com>
2021-01-24 20:21:35 -0800

    switch state and shape

--- flyobjs.py
+++ flyobjs.py
@@ -15,88 +15,137 @@ MSLE_LIFE = 8
 MSLE_TURN = 2.5 # missile turn degree
 HIT_RANGE = 15
 
-team_color = {0: "black", 1: "blue", 2: "green", 3: "Orange"}
-ST_READY = 0
-ST_BORN = 1
-ST_FLY = 2
-ST_EXPLODE = 3
-ST_TURBO = 4
+teams = {"black", "blue", "green", "orange"}
+states = {"READY": "static", "BORN": "dynamic", "FLY": "static", "EXPLODE": "dynamic", "TURBO": "static"}
+
+FPS = 50   # 50 FPS per second
+ticks = 0  # screen updating ticks, there is FPS ticks every second.
 
 class Flyobj(Turtle):
     """ flying turtle class """
-    def __init__(self, team_id):
+    def __init__(self, team):
         super().__init__()
-        self.state = ST_READY  # READY:0, BORN:1, FLY:2, EXPLODE:3, TURBO:4
-        self.state_start = None  # start time of the state
-        self.team_id = team_id
-        self.fillcolor(team_color[self.team_id])
-        self.shapes = {}  # shape dict
-        self.speeds = {}  # state:speed dict
+        self.state = "READY"  # READY:0, BORN:1, FLY:2, EXPLODE:3, TURBO:4
+        self.state_start = 0   # start tick of the state
+        self.team = team
+        self.fillcolor(self.team)
+        self.shapes = []  # shape set
+        self.paces = dict()  # state:speed dict
         self.up()
 
-    def reg_shape(self, state, pic_prefix):
-        """shape is string in dict, register all shapes used by this obj"""
-        pass
+    def reg_static_shape(self, state, polys):
+        s = Shape("compound")
+        c = self.team
+        for poly in polys:
+            s.addcomponent(poly, c, c)
+        shape_id = self.team + state
+        register_shape(shape_id, s)
+        self.shapes.insert(0, shape_id)
 
-    def get_shape(self, state, st_time):
-        """get shape name from dict"""
-        pass
+    def reg_dynamic_shape(self, state, prefix, cnt):
+        """pics/plane_explode_01.gif"""
+        for i in range(cnt):
+            image = "pics/%s_%s_%02d.gif" % (prefix, state.lower(), i)
+            if image not in self.shapes:  # other plane may reg it already
+                print("reg image:" + image)
+                register_shape(image)
+                self.shapes.append(image)
+
+    def get_shape(self, state, st_ticks):
+        """get shape from list"""
+        shape_id = self.team + state
+        if shape_id in self.shapes:
+            return shape_id
+        for s in self.shapes:
+            image = "%s_%02d.gif" % (state.lower(), st_ticks % 10)
+            if s.endswith(image):
+                return s
+        return "triangle"  # fall back shape
+
+    def switch_state(self, state):
+        if state == self.state:
+            return
+        self.state = state
+        self.state_start = ticks
+        if self.state == "READY":
+            self.hideturtle()
+            return
+        # change shape now
+        self.shape(self.get_shape(self.state, ticks - self.state_start))
 
     def do_job(self):
         """Move, change shape, check collision, switch state"""
-        if self.state != ST_READY:
-            self.fd(self.speeds[self.state])
-        else:
-            self.hideturtle()
+        if self.state != "READY":
+            self.fd(self.paces[self.state])
+        if self.state == "EXPLODE":
+            self.shape(self.get_shape(self.state, ticks - self.state_start))
 
 
 class Plane(Flyobj):
     """
     Plane can turbo speed, has clone count, has life limit
-    states = ["READY", "FLYING", "TURBO", "EXPLODE" ]
+    states = ["READY", "FLY", "TURBO", "EXPLODE" ]
     """""
-    def __init__(self, team_id):
-        super().__init__(team_id)
+    def __init__(self, team):
+        super().__init__(team)
         self.clone = 5
         self.life = 100  # 0 ~ 100 percent
-        self.state = ST_FLY
-        self.shape("turtle")
-        self.speeds = {ST_READY: 0, ST_BORN: 0, ST_FLY: PLANE_SPEED, ST_TURBO: TURBO_SPEED, ST_EXPLODE: PLANE_SPEED}
+        #self.state = "FLY"
+        #self.shape("turtle")
+        self.paces = {"READY": 0, "BORN": 0, "FLY": PLANE_SPEED, "TURBO": TURBO_SPEED, "EXPLODE": PLANE_SPEED}
+        self.reg_static_shape("FLY", [((0, 20), (-4, -8), (0, -6), (4, -8)), ((0, 16), (-12, -2), (12, -2))])
+        self.reg_static_shape("TURBO", [((0, 20), (-4, -8), (0, -6), (4, -8)), ((0, 16), (-12, -2), (12, -2))])
+        self.reg_dynamic_shape("EXPLODE", "", 10)
+
+        print(self.shapes)
 
     def do_job(self):
         """
         FLYING/TURBO/RESTORE-> Get keypad input,
         EXPLODE-> Change shape
         """
+        if (ticks / FPS) < 3:
+            self.switch_state("TURBO")
+        elif (ticks / FPS) < 6:
+            self.switch_state("FLY")
+        else:
+            self.switch_state("EXPLODE")
         super().do_job()
 
 
 class Missile(Flyobj):
-    """states = ["FLYING", "EXPLODE" ]"""
-    def __init__(self, team_id, target_id):
-        super().__init__(team_id)
-        self.state = ST_FLY
-        self.launch_time=0
-        self.shape("circle")
-        self.speeds = {ST_READY: 0, ST_BORN: 0, ST_FLY: MSLE_SPEED, ST_EXPLODE: MSLE_SPEED}
+    """states = ["FLY", "EXPLODE" ]"""
+    def __init__(self, team, target_team):
+        super().__init__(team)
+        self.state = "FLY"
+        self.reg_static_shape("FLY", [((-1, 0), (0, 12), (1, 0))])
+        self.reg_dynamic_shape("EXPLODE", "", 10)
+        self.paces = {"READY": 0, "BORN": 0, "FLY": MSLE_SPEED, "EXPLODE": MSLE_SPEED}
 
     def do_job(self):
         """
         FLYING-> Turn to target, check hit,
         EXPLODE-> Change shape
         """
+        if (ticks / FPS) < 5:
+            self.switch_state("FLY")
+        elif (ticks / FPS) < 6:
+            self.switch_state("EXPLODE")
+        else:
+            self.switch_state("READY")
+
         #  let's Flyobj do_job
         super().do_job()
 
 
 class Bullet(Flyobj):
-    """states = ["FLYING"]"""
+    """states = ["FLY"]"""
     def __init__(self):
-        super().__init__(0)
-        self.state = ST_FLY
+        super().__init__("black")
+        self.state = "FLY"
         self.fire_time=0
         self.shape("circle")
-        self.speeds = {ST_READY: 0, ST_BORN: 0, ST_FLY: BLT_SPEED}
+        self.paces = {"READY": 0, "BORN": 0, "FLY": BLT_SPEED}
 
     def do_job(self):
         """FLYING-> check hit to all planes, missiles"""
@@ -104,6 +153,7 @@ class Bullet(Flyobj):
 
 
 def run():
+    global ticks
     for p in planes:
         p.left(3)
         p.do_job()
@@ -114,7 +164,9 @@ def run():
         b.do_job()
 
     update()
-    ontimer(run, 30) # 33.3 frame per second
+    ticks = ticks + 1
+    ontimer(run, int(1000/FPS))  # FPS frame per second
+
 
 def main():
     global planes, missiles, bullets
@@ -123,11 +175,11 @@ def main():
     bullets = []
     tracer(False)
     for i in range(10):
-        p = Plane(1)
+        p = Plane("blue")
         p.setheading(i * 30)
         planes.append(p)
     for i in range(10):
-        m = Missile(2, 1)
+        m = Missile("green", "blue")
         m.setheading(i * 20)
         missiles.append(m)
     for i in range(10):
